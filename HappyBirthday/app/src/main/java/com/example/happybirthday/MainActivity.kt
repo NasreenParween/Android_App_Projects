e0dd2ab4bb2985c330c69a64d15a941c459c1151
NasreenParween <nasreen999786@gmail.com>
2022-05-06 11:44:48 +0530

    my 1st project in kotlin

---
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.happybirthday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.graphics.createBitmap
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
